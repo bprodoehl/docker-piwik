2e5e95be72ea3d1b0bda492f5ef45d739136020c
Pierre-Jean Leger <Caligone@users.noreply.github.com>
2015-03-01 10:24:38 +0100

    Create certs directory

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -10,6 +10,7 @@ if [ ! -f /etc/nginx-certs/server.pem ] || [ ! -f /etc/nginx-certs/server.key ];
     openssl req -new -key server.key -out server.csr \
     -subj "/C=XX/ST=X/L=X/O=X/OU=X/CN=example.com"
     openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+    mkdir /etc/nginx-certs/
     cp server.key /etc/nginx-certs/server.key
     cp server.crt /etc/nginx-certs/server.pem
 fi
